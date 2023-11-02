b8975fe1ef6745bfe0aab48569980f9680d719fe
thariga555 <136608456+thariga555@users.noreply.github.com>
2023-11-02 11:24:07 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,33 @@
+export const quiz = {
+    topic:"javascript",
+    level:"beginner",
+    totalQuestions:4,
+    perQuestionScore:5,
+    questions: [
+        {
+         question:'which function is used to serialize an object into a JSON string in javascript?',
+         choices:['stringify()','parse()','convert()','None of the above'],
+         type:'MCQs',
+         correctAnswer:'stringify()',
+    },
+    {
+        question:'which of the following keywords is used to define a variable in javascript?',
+         choices:['var','let','var and let','None of the above'],
+         type:'MCQs',
+         correctAnswer:'var and let',
+    },
+    {
+        question:'which of the following methods can be used to display data in some form using javascript?',
+         choices:['document.write()','console.log()','window.alert()','All of the above'],
+         type:'MCQs',
+         correctAnswer:'All of the above',
+    },
+    {
+        question:'How can a datatype be declared to be a constant type?',
+         choices:['const','var','let','constant'],
+         type:'MCQs',
+         correctAnswer:'const',
+    },
+],
+}
+
